e47417a98f88c9aa8e44a9919d61bc504399571d
ElPay-KLS <48352036+ElPay-KLS@users.noreply.github.com>
2019-03-21 12:47:37 +0530

    Add files via upload

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Net.Http.Headers;
+
+namespace WebDate
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //Configure Web API to return Json
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+        }
+
+     
+    }
+    }
+
---
+++ Controllers/DatesController.cs
@@ -0,0 +1,134 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebDate;
+
+namespace WebDate.Controllers
+{
+    public class DatesController : ApiController
+    {
+        private WebDateDBEntities db = new WebDateDBEntities();
+
+        // GET: api/Dates
+        public IQueryable<Date> GetDates()
+        {
+            return db.Dates;
+        }
+
+        // GET: api/Dates/5
+        [ResponseType(typeof(Date))]
+        public IHttpActionResult GetDate(int id)
+        {
+            Date date = db.Dates.Find(id);
+            if (date == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(date);
+        }
+
+        // PUT: api/Dates/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDate(int id, Date date)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != date.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(date).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Dates
+        [ResponseType(typeof(Date))]
+        public IHttpActionResult PostDate(Date date)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Dates.Add(date);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = date.ID }, date);
+        }
+        public class con
+        {
+            [JsonConverter(typeof(OnlyDateConverter))]
+            public DateTime Bar { get; set; }
+        }
+
+        public class OnlyDateConverter : IsoDateTimeConverter
+        {
+            public OnlyDateConverter()
+            {
+                DateTimeFormat = "yyyy-MM-dd";
+            }
+        }
+
+
+        // DELETE: api/Dates/5
+        [ResponseType(typeof(Date))]
+        public IHttpActionResult DeleteDate(int id)
+        {
+            Date date = db.Dates.Find(id);
+            if (date == null)
+            {
+                return NotFound();
+            }
+
+            db.Dates.Remove(date);
+            db.SaveChanges();
+
+            return Ok(date);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DateExists(int id)
+        {
+            return db.Dates.Count(e => e.ID == id) > 0;
+        }
+    }
+}
